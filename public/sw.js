c1a4628a1f4642b3de9aa6e46f8b1fc1a6c56f5a
ricoandrade1 <ricoandrade01@gmail.com>
2025-01-28 22:58:42 +0000

    Add PWA functionality

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'document',
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'style' || request.destination === 'script',
+  new workbox.strategies.StaleWhileRevalidate()
+);
